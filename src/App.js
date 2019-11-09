7b09a0f6adf2f40048407675145944243f149cf3
Colin Coutts <colintcoutts@gmail.com>
2019-11-09 15:29:15 -0800

    add filteredMonsters to render only monster names that are included in searchfield

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className='App'>
         <input
@@ -26,7 +30,7 @@ class App extends Component {
           placeholder='search monsters'
           onChange={event => this.setState({ searchField: event.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
